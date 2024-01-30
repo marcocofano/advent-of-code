ce4de517f744d190a0a86f70758c49f47f5ab661
marcocofano <m.cofano@gmail.com>
2024-01-30 12:16:55 +0100

    Day 14 in rust. Part 2 and Part 1 refactor

--- y2023/rust/src/bin/14.rs
+++ y2023/rust/src/bin/14.rs
@@ -1,10 +1,18 @@
-use std::fmt;
-
 use itertools::Itertools;
-
+use std::collections::HashMap;
+use std::fmt;
 advent_of_code::solution!(14);
 
+enum Direction {
+    North,
+    West,
+    South,
+    East,
+}
+
+const DIRECTIONS: [Direction; 4] = [Direction::North, Direction::West, Direction::South, Direction::East];
 type Grid = Vec<char>;
+#[derive(Clone)]
 struct Platform {
     grid: Grid,
     size: usize,
@@ -28,27 +36,36 @@ impl fmt::Debug for Platform {
 }
 
 impl Platform {
-    
+
+    fn to_hashable(&self) -> Vec<u128> {
+        // Azarias version
+        let mut result = vec![];
+        for row in 0..self.size {
+            let row_slice = self.get_row_slice(row);
+            result.push(row_slice.iter().enumerate().fold(0u128, |acc, (x, &c)| {
+                if c == 'O' {
+                    acc | (1 << x)
+                } else {
+                    acc
+                }
+            }));
+        }
+        result
+    }
+
+    // More expensive representation. it does not matter in terms of speed.
+    #[allow(dead_code)]
+    fn to_hashable_2(&self) -> Vec<usize> {
+        return self
+            .grid
+            .iter()
+            .positions(|&item| item == 'O')
+            .collect_vec();
+    }
+
     fn get_row_slice(&self, row: usize) -> &[char] {
         let start = row * self.size;
         &self.grid[start..start + self.size]
-    }
-
-    fn rotate_clockwise(&mut self) {
-        for layer in 0..self.size / 2 {
-            let first = layer;
-            let last = self.size - 1 - layer;
-
-            for i in first..last {
-                let offset = i - first;
-
-                let top = self.get(first, i);
-                self.set(first, i, self.get(last - offset, first));
-                self.set(last - offset, first, self.get(last, last - offset));
-                self.set(last, last - offset, self.get(i, last));
-                self.set(i, last, top);
-            }
-        }
     }
 
     fn get(&self, row: usize, col: usize) -> char {
@@ -60,31 +77,90 @@ impl Platform {
     }
 
     fn count_weight(&self) -> usize {
-        let mut weight = 0;
-        for row_num in 0..self.size {
-            let row = self.get_row_slice(row_num);
-            weight += row
-                .iter()
-                .enumerate()
-                .fold(0, |sum, (i, e)| if *e == 'O' { sum + i + 1 } else { sum });
-        }
+        let weight = (0..self.size).fold(0, |sum, r| {
+            sum + (self.size - r)
+                * self
+                    .get_row_slice(r)
+                    .iter()
+                    .filter(|&item| item == &'O')
+                    .count()
+        });
         weight
     }
 
-    fn tilt_rocks(&mut self) {
-        for row_n in 0..self.size {
-            let mut first_available = self.size - 1;
-            for col_n in (0..self.size).rev() {
-                match self.get(row_n, col_n) {
-                    '#' => first_available = col_n.saturating_sub(1),
-                    'O' => {
-                        self.set(row_n, col_n, '.');
-                        self.set(row_n, first_available, 'O');
-                        first_available = first_available.saturating_sub(1);
+    fn tilt_rocks(&mut self, direction: &Direction) {
+        match direction {
+            Direction::East => {
+                for row_n in 0..self.size {
+                    let mut first_available = self.size - 1;
+                    for col_n in (0..self.size).rev() {
+                        match self.get(row_n, col_n) {
+                            '#' => first_available = col_n.saturating_sub(1),
+                            'O' => {
+                                self.set(row_n, col_n, '.');
+                                self.set(row_n, first_available, 'O');
+                                first_available = first_available.saturating_sub(1);
+                            }
+                            _ => {}
+                        }
                     }
-                    _ => {}
                 }
             }
+            Direction::West => {
+                for row_n in 0..self.size {
+                    let mut first_available = 0;
+                    for col_n in 0..self.size {
+                        match self.get(row_n, col_n) {
+                            '#' => first_available = col_n + 1,
+                            'O' => {
+                                self.set(row_n, col_n, '.');
+                                self.set(row_n, first_available, 'O');
+                                first_available = first_available + 1;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            }
+            Direction::South => {
+                for col_n in 0..self.size {
+                    let mut first_available = self.size - 1;
+                    for row_n in (0..self.size).rev() {
+                        match self.get(row_n, col_n) {
+                            '#' => first_available = row_n.saturating_sub(1),
+                            'O' => {
+                                self.set(row_n, col_n, '.');
+                                self.set(first_available, col_n, 'O');
+                                first_available = first_available.saturating_sub(1);
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+
+            }
+            Direction::North => {
+                for col_n in 0..self.size {
+                    let mut first_available = 0;
+                    for row_n in 0..self.size {
+                        match self.get(row_n, col_n) {
+                            '#' => first_available = row_n + 1,
+                            'O' => {
+                                self.set(row_n, col_n, '.');
+                                self.set(first_available, col_n, 'O');
+                                first_available = first_available + 1;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn do_cycle(&mut self) {
+        for dir in DIRECTIONS.iter() {
+            self.tilt_rocks(dir);
         }
     }
 }
@@ -101,16 +177,32 @@ fn parse_lines(input: &str) -> (Grid, usize) {
 
 pub fn part_one(input: &str) -> Option<usize> {
     let (grid, size) = parse_lines(input);
-    let mut platform = Platform {
-        grid,
-        size,
-    };
-    platform.rotate_clockwise();
-    platform.tilt_rocks();
+    let mut platform = Platform { grid, size };
+    platform.tilt_rocks(&Direction::North);
     Some(platform.count_weight())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<usize> {
+    let (grid, size) = parse_lines(input);
+    let mut platform = Platform { grid, size };
+    let mut history_map = HashMap::new();
+    let end = 1_000_000_000 - 1;
+    for i in 0..end {
+        platform.do_cycle();
+        let platform_hash = platform.to_hashable();
+        if let Some(&position) = history_map.get(&platform_hash) {
+            let cycle_size = i - position;
+            let rest = end - i;
+            let final_steps = rest % cycle_size;
+            for _ in 0..final_steps {
+                platform.do_cycle()
+            }
+            let result = platform.count_weight();
+            return Some(result);
+        } else {
+            history_map.insert(platform_hash, i);
+        }
+    }
     None
 }
 
@@ -127,6 +219,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(64));
     }
 }
